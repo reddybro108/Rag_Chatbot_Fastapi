1346461543983ed529c7b41fdbd171d45c18f8be
reddybro108 <mail2amolreddy@gmail.com>
2025-05-28 15:06:43 +0530

    dummy folder added for testing4

--- app/main.py
+++ app/main.py
@@ -7,13 +7,12 @@ nltk.download('punkt')
 nltk.download('stopwords')
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from model import model, vectorizer
 from preprocess import clean_text
 from fastapi import FastAPI
 
+from app.routes import router  # instead of from routes import router
+from app.model import model, vectorizer
 
-from fastapi import FastAPI
-from routes import router
 
 app = FastAPI()
 app.include_router(router)
@@ -45,19 +44,3 @@ def predict_intent(query: Query):
     prediction = model.predict(vectorized)[0]
     return {"intent": prediction}
 
-# from fastapi import FastAPI
-# from pydantic import BaseModel
-# from app.model import model, vectorizer
-# from app.preprocess import clean_text
-
-# app = FastAPI()
-
-# class Query(BaseModel):
-#     user_input: str
-
-# @app.post("/predict/")
-# def predict_intent(query: Query):
-#     cleaned = clean_text(query.user_input)
-#     vectorized = vectorizer.transform([cleaned])
-#     prediction = model.predict(vectorized)[0]
-#     return {"intent": prediction}
