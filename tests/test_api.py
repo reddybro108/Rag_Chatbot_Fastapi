ecd93b6eebbf0c607af2d103878e5dfbc5fbc577
reddybro108 <mail2amolreddy@gmail.com>
2025-05-28 12:59:26 +0530

    dummy folder added for testing1

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,12 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+
 from fastapi.testclient import TestClient
 from main import app
+
+
 
 client = TestClient(app)
 
